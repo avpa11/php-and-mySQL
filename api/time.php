ca60562acaca626c8a74ca964e7acea044f1f1a7
Alina Pak <47726938+avpa11@users.noreply.github.com>
2019-03-14 00:21:14 -0700

    Update time.php

--- api/time.php
+++ api/time.php
@@ -1,6 +1,6 @@
 <?php
 
-    // API 
+    // TimeZone API 
     $timezone = $_GET['zone'];
     $url = 'http://api.timezonedb.com/v2/get-time-zone?key=PTYIG6CV3HI5&format=json&by=zone&zone='.$timezone;
     $json = file_get_contents($url);
